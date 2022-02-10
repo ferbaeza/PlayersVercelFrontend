d47167a402c313538289794fb152cdf1181b8e31
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2022-02-10 11:19:09 +0100

    update

--- js/edit.js
+++ js/edit.js
@@ -1,30 +1,29 @@
 function createNode(element) {
-    return document.createElement(element);
+  return document.createElement(element);
 }
 
 function append(parent, el) {
   return parent.appendChild(el);
 }
 
-
 async function fetchProduct(id) {
-    const response = await fetch(
-      "https://footballexpressherokuvercel.herokuapp.com/players/"+id,
-      {		
-        method: "GET",
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json"		  
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        let player = data.data;
-        console.log(player);
-	
-	 try {	 
+  const response = await fetch(
+    "https://footballexpressherokuvercel.herokuapp.com/players/" + id,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let player = data.data;
+      console.log(player);
+
+      try {
         if (player != null) {
           document.getElementById("id").value = player._id;
           document.getElementById("name").value = player.name;
@@ -33,96 +32,99 @@ async function fetchProduct(id) {
           document.getElementById("position").value = player.position;
           document.getElementById("team").value = player.team;
         }
-	  
-	}
-	catch (e) {
-	   // sentencias para manejar cualquier excepción
-	   console.log(e); // pasa el objeto de la excepción al manejador de errores
-	}
-      })
-      .catch((error) => console.log(error));
-  }
+      } catch (e) {
+        // sentencias para manejar cualquier excepción
+        console.log(e); // pasa el objeto de la excepción al manejador de errores
+      }
+    })
+    .catch((error) => console.log(error));
+}
 
 function getParameterByName(name, url = window.location.href) {
-    console.log(url);
-    
-    name = name.replace(/[\[\]]/g, '\\$&');
-    
-    console.log(name);
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  console.log(url);
+
+  name = name.replace(/[\[\]]/g, "\\$&");
+
+  console.log(name);
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-  }
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
-fetchProduct(getParameterByName('id'));
+fetchProduct(getParameterByName("id"));
 
-
-
-function selects(){
-    const positions = ["GK", "DF", "MF", "FW"];
-    const team = [
+function selects() {
+  const positions = ["GK", "DF", "MF", "FW"];
+  const team = [
     "REAL MADRID",
     "FC BARCELONA",
     "PSG",
     "BAYERN MUNICH",
     "BORUSSIA DORTMUND",
-    ];
+  ];
 
-    let select = document.getElementById("position");
-    for(let position of positions){
+  let select = document.getElementById("position");
+  for (let position of positions) {
+    let op = createNode("option");
 
-        let op = createNode('option');
+    op.innerHTML = position;
 
-        op.innerHTML=position;
+    append(select, op);
+  }
 
-        append(select,op);
-    }
+  let select2 = document.getElementById("team");
+  for (let teamm of team) {
+    let op = createNode("option");
 
-    let select2 = document.getElementById("team");
-    for(let teamm of team){
+    op.innerHTML = teamm;
 
-        let op = createNode('option');
-
-        op.innerHTML=teamm;
-
-        append(select2,op);
-    }
+    append(select2, op);
+  }
 }
 
 selects();
 
 const save = document.getElementById("save");
-save.addEventListener("click",fetchEditProduct);
+save.addEventListener("click", fetchEditProduct);
 
 async function fetchEditProduct() {
-	const idField = document.getElementById("idplayer").value;
-	const nameField = document.getElementById("name").value;
-	const surnameField = document.getElementById("surname").value;
-	const ageField = document.getElementById("age").value;
-    const positionField = document.getElementById("position").value;
-    const teamField = document.getElementById("team").value;
+  const idField = document.getElementById("idplayer").value;
+  const nameField = document.getElementById("name").value;
+  const surnameField = document.getElementById("surname").value;
+  const ageField = document.getElementById("age").value;
+  const positionField = document.getElementById("position").value;
+  const teamField = document.getElementById("team").value;
 
-	//console.log(nameField + " " + priceField + " " + categoryField);
-	
-	const newPlayer = {_id: idField ,name: nameField, surname: surnameField, age:ageField, position: positionField, team: teamField};
-	
-    const response = await fetch(
-        "https://footballexpressherokuvercel.herokuapp.com/players/"+ idField + "?_method=PUT",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-		body: JSON.stringify(newPlayer)
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        window.location.href = "index.html";
-      })
-      .catch((error) => console.log(error));
-  }
+  //console.log(nameField + " " + priceField + " " + categoryField);
+
+  const newPlayer = {
+    _id: idField,
+    name: nameField,
+    surname: surnameField,
+    age: ageField,
+    position: positionField,
+    team: teamField,
+  };
+
+  const response = await fetch(
+    "https://footballexpressherokuvercel.herokuapp.com/players/" +
+      idField +
+      "?_method=PUT",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlayer),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      window.location.href = "index.html";
+    })
+    .catch((error) => console.log(error));
+}
--- js/newplayer.js
+++ js/newplayer.js
@@ -1,6 +1,5 @@
 const btnNew = document.getElementById("submit");
-btnNew.addEventListener("click",fetchNewPlayer);
-
+btnNew.addEventListener("click", fetchNewPlayer);
 
 async function fetchNewPlayer() {
   const nameField = document.getElementById("name").value;
@@ -8,27 +7,32 @@ async function fetchNewPlayer() {
   const ageField = document.getElementById("age").value;
   const positionField = document.getElementById("position").value;
   const teamField = document.getElementById("team").value;
-  const newPlayer = {name: nameField, surname: surnameField, age:ageField, position:positionField, team:teamField};
+  const newPlayer = {
+    name: nameField,
+    surname: surnameField,
+    age: ageField,
+    position: positionField,
+    team: teamField,
+  };
 
-
-    const response = await fetch(
-      "https://footballexpressherokuvercel.herokuapp.com/players/",
-      {				
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': "application/json"
-        },
-		body: JSON.stringify(newPlayer)
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        window.location.href = "index.html";        
-      })
-      .catch((error) => console.log(error));
-  }
+  const response = await fetch(
+    "https://footballexpressherokuvercel.herokuapp.com/players/",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlayer),
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      window.location.href = "index.html";
+    })
+    .catch((error) => console.log(error));
+}
 function createNode(element) {
   return document.createElement(element);
 }
@@ -37,43 +41,33 @@ function append(parent, el) {
   return parent.appendChild(el);
 }
 
-
-
-
-
-
-
-
-
-  function selects(){
-    const positions = ["GK", "DF", "MF", "FW"];
-    const team = [
+function selects() {
+  const positions = ["GK", "DF", "MF", "FW"];
+  const team = [
     "REAL MADRID",
     "FC BARCELONA",
     "PSG",
     "BAYERN MUNICH",
     "BORUSSIA DORTMUND",
-    ];
+  ];
 
-    let select = document.getElementById("position");
-    for(let position of positions){
+  let select = document.getElementById("position");
+  for (let position of positions) {
+    let op = createNode("option");
 
-        let op = createNode('option');
+    op.innerHTML = position;
 
-        op.innerHTML=position;
+    append(select, op);
+  }
 
-        append(select,op);
-    }
+  let select2 = document.getElementById("team");
+  for (let teamm of team) {
+    let op = createNode("option");
 
-    let select2 = document.getElementById("team");
-    for(let teamm of team){
+    op.innerHTML = teamm;
 
-        let op = createNode('option');
-
-        op.innerHTML=teamm;
-
-        append(select2,op);
-    }
+    append(select2, op);
+  }
 }
 
 selects();
